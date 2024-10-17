998ab4696b15745c26692fe7fd028f333dd71a39
Tayla Ball <tayla.ball@hotmail.com>
2024-10-17 09:51:44 +0000

    Add: calculate correct answer for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,22 @@ function checkAnswers() {
     
 }
 
-function calculateCorrectAnswer() {
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer
+ */
 
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operamd1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Uimplemented operator ${operator}.Aborting!`;
+    }
 }
 
 function incrementScore() {
